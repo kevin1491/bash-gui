16c77f8883634c63496511a106e01b6ab5f7365b
kevin1491 <kevin1491@163.com>
2020-04-30 09:26:59 +0800

    add func

--- main.sh
+++ main.sh
@@ -50,7 +50,6 @@ board_init()
     color[$i]=$cwhite;
     bcolor[$i]=$bblack;
   done
-  board[100]=4;
 }
 
 board_random()
@@ -62,19 +61,58 @@ board_random()
   done
 }
 
+board_black()
+{
+  for ((i=0;i<SCLINES*SCCOLNS;i++));do
+    #board[$i]=2;
+    color[$i]=$(($cblack+2));
+    bcolor[$i]=$bblack;
+  done
+}
+
 board_flush()
 {
   clear;
   local i=0;
   for((i=0;i<SCLINES*SCCOLNS;i++));do
-      x=$(($i/SCCOLNS));
-      y=$(($i%SCCOLNS));
+      y=$(($i/SCCOLNS));
+      x=$(($i%SCCOLNS));
       #echo "$i $x $y ${bcolor[$i]} ${color[$i]}"
       if [ ${board[$i]} != 0 ];then
-        echo -ne "\033[$x;$yH\033[${bcolor[$i]};${color[$i]}m${board[$i]}\033[0m";
+        echo -ne "\033[${y};${x}H\033[${bcolor[$i]};${color[$i]}m${board[$i]}\033[0m";
       else 
-        echo -ne "\033[$x;$yH\033[${bcolor[$i]};${color[$i]}m \033[0m";
+        echo -ne "\033[${y};${x}H\033[${bcolor[$i]};${color[$i]}m \033[0m";
       fi
+
+      if [ $x == 0 ];then
+        echo "";
+      fi
+  done
+}
+
+#board_flush_line 10
+board_flush_line()
+{
+  for((i=0;i<SCCOLNS;i++));do
+    echo -ne "\033[${1};${i}H\033[${bcolor[$i]};${color[$i]}m${board[$i]}\033[0m";
+  done
+}
+
+#board_flush_col 10
+board_flush_col()
+{
+  for((i=0;i<SCLINES;i++));do
+    echo -ne "\033[${i};${1}H\033[${bcolor[$i]};${color[$i]}m${board[$i]}\033[0m";
+  done
+}
+
+#board_flush_rect 10 10 10 10
+board_flush_rect()
+{
+  for((i=0;i<$3;i++));do
+    for((j=0;j<$4;j++));do
+      echo -ne "\033[$(($2+$j));$(($1+$i))H\033[${bcolor[$(($i+$j*SCCOLNS))]};${color[$(($i+$j*SCCOLNS))]}m${board[$(($i+${j}*SCCOLNS))]}\033[0m";
+    done
   done
 }
 
@@ -87,15 +125,22 @@ set_backgroud_color()
   done
 }
 
-
-
-
 #set_pix_char 10 10 "k"
 set_pix_char()
 {
   board[$(($2*SCCOLNS+$1))]=$3;
 }
-  
+
+#brush_rect 40 10 100 100 $ccyan $bwhite
+brush_rect()
+{
+   for((i=0;i<$3;i++));do
+    for((j=0;j<$4;j++));do
+      color[$(($1+$2*SCCOLNS+$i+$j*SCCOLNS))]=$6;
+      bcolor[$(($1+$2*SCCOLNS+$i+$j*SCCOLNS))]=$7;
+    done
+  done
+}
 
 #TODO debug this
 move_board()
@@ -119,14 +164,16 @@ draw_box()
 board_main() 
 {
    board_init;
+#   board_random; 
+#   board_flush;
+   board_black;
+   #board_flush_rect 10 10 10 10;
+   
+#   brush_rect 40 5 50 40 $ccyan $bwhite
    board_flush;
-   #sleep 3
-   board_random; 
-   board_flush;
-   sleep 1;
-   set_pix_char 1 10 "k";
-   move_board;
-   board_flush;
+#   set_pix_char 1 10 "k";
+#   move_board;
+   sleep 100;
    board_exit;
 }
 
